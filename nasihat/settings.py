522838813bdb62cc1d2aaff9007ceb55660599f1
Shukrullo <94306881+Shukrullo2@users.noreply.github.com>
2024-11-15 11:51:10 +0500

    Db reverted to sqlite

--- nasihat/settings.py
+++ nasihat/settings.py
@@ -108,19 +108,19 @@ WSGI_APPLICATION = "nasihat.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASES = {
-    "default": dj_database_url.parse(
-        "postgresql://shukrullo:FORdoq6J4YgE3R1DkTxd2umL54KrG5If@dpg-csi6ukbgbbvc73fe6cmg-a.oregon-postgres.render.com/tavsiya"
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(
+#         "postgresql://shukrullo:FORdoq6J4YgE3R1DkTxd2umL54KrG5If@dpg-csi6ukbgbbvc73fe6cmg-a.oregon-postgres.render.com/tavsiya"
+#     )
+# }
 
 
 
